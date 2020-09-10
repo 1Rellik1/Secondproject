6887c4cb5635d6075a66107afb8ed12d92b9d66d
Alex <Alexzz123@yandex.ru>
2020-09-10 14:32:03 +0300

    Task 4
    Added task 4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,31 +3,32 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
-        ////////////Task1//////////////
-        Shapetest one=new Shapetest();
-
-    /////////////Task2//////////////
-        Scanner scan=new Scanner(System.in);
-        String color;
-        int size;
-        color=scan.nextLine();
-        size= scan.nextInt();
-        Ball first=new Ball(size,color);
-        System.out.println(first.toString());
-
-
-        //////////Task3///////////////////
-        String nameOfBook,author;
-        int date,pages;
-        nameOfBook= scan.nextLine();;
-        author= scan.nextLine();
-        date= scan.nextInt();
-        pages= scan.nextInt();
-        Book firstBook= new Book(nameOfBook,author,date,pages);
-        System.out.println(firstBook.toString());
+//        ////////////Task1//////////////
+//        Shapetest one=new Shapetest();
+//
+//    /////////////Task2//////////////
+//        Scanner scan=new Scanner(System.in);
+//        String color;
+//        int size;
+//        color=scan.nextLine();
+//        size= scan.nextInt();
+//        Ball first=new Ball(size,color);
+//        System.out.println(first.toString());
+//
+//
+//        //////////Task3///////////////////
+//        String nameOfBook,author;
+//        int date,pages;
+//        nameOfBook= scan.nextLine();;
+//        author= scan.nextLine();
+//        date= scan.nextInt();
+//        pages= scan.nextInt();
+//        Book firstBook= new Book(nameOfBook,author,date,pages);
+//        System.out.println(firstBook.toString());
         /////////Task4//////////////////
+
+        PitomnikSobak firstPitomnik= new PitomnikSobak();
 
     }
 
--- src/com/company/PitomnikSobak.java
+++ src/com/company/PitomnikSobak.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class PitomnikSobak {
     private Dog[] mas;
 
-    public PitomnikSobak(Dog[] mas) {
+    public PitomnikSobak() {
         int n;
          String NameOfDog;
          int Age;
@@ -14,7 +14,9 @@ public class PitomnikSobak {
         n=scan.nextInt();
         this.mas = new Dog[n];
         for(int i=0;i<n;i++){
+            System.out.print("Введите имя собаки: ");
             NameOfDog=scan.nextLine();
+            System.out.print("Введите возраст собаки: ");
             Age= scan.nextInt();
             Dog first=new Dog();
             first.setAge(Age);
