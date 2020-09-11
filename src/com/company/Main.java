256315be4d5c234efdcf08f68b9740c296d31b9b
Alex <Alexzz123@yandex.ru>
2020-09-11 18:47:41 +0300

    Redid main
    Main was updted

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,28 +4,28 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-    ////////////Task1//////////////
+        ////////////Task1//////////////
        Shapetest one=new Shapetest();
 
     /////////////Task2//////////////
         Scanner scan=new Scanner(System.in);
-       String color;
-      int size;
-     color=scan.nextLine();
-      size= scan.nextInt();
-      Ball first=new Ball(size,color);
-     System.out.println(first.toString());
+        String color;
+        int size;
+       color=scan.nextLine();
+        size= scan.nextInt();
+        Ball first=new Ball(size,color);
+        System.out.println(first.toString());
 
 
-       //////////Task3///////////////////
+        //////////Task3///////////////////
         String nameOfBook,author;
         int date,pages;
-        nameOfBook= scan.nextLine();
+        nameOfBook= scan.nextLine();;
         author= scan.nextLine();
-       date= scan.nextInt();
-       pages= scan.nextInt();
+        date= scan.nextInt();
+        pages= scan.nextInt();
         Book firstBook= new Book(nameOfBook,author,date,pages);
-       System.out.println(firstBook.toString());
+        System.out.println(firstBook.toString());
         /////////Task4//////////////////
 
         PitomnikSobak firstPitomnik= new PitomnikSobak();
